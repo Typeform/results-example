4ec391ee04aae12ba5246c69d5152f7dac5e176e
Denys Honsiorovskyi <honsiorovskyi@gmail.com>
2017-06-12 18:38:22 +0200

    Initial commit

---
+++ handlers.js
@@ -0,0 +1,82 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    indexHandler: function(req, res) {
+        if (!req.isAuthenticated()) {
+            return res.end(`
+            <body>
+                <h3>Hello stranger!</h3>
+                <p>You're not authenticated, you need to <a href="/login">authenticate via Typeform</a>.
+            </body>
+            `)
+        }
+
+        let data = JSON.stringify(req.user);
+        return res.end(`
+        <body>
+            <h3>Hello, $username!</h3>
+            <p>Here's your profile:</p><p style="color: blue;">${data}</p>
+            <p>Maybe you want to <a href="/logout">log out</a>?</p>
+        </body>
+        `);
+    },
+
+    displayResultsHandler: function(req, res) {
+        fetch(`https://api.typeform.com/forms/${req.params.id}/responses`, {
+            headers: {
+                Authorization: `Bearer ${req.user.access_token}`,
+            }
+        })
+        .then(res => res.json())
+        .then(d => {
+            let xdata = [];
+            let ydata = [];
+            d.items.forEach(item => {
+                if (item.answers) {
+                    if (item.answers[0].type == "number") {
+                        xdata.push(xdata[xdata.length-1]+1 || 1);
+                        ydata.push(item.answers[0].number);
+                    }
+                }
+            })
+
+            res.end(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
+            </head>
+            <body>
+                <canvas id="canvas" width="800" height="400"></canvas>
+                <script>
+                    var data = {
+                        labels: ${JSON.stringify(xdata)},
+                        datasets: [
+                            {
+                                label: "Answer",
+                                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                                borderColor: 'rgba(54, 162, 235, 1)',
+                                borderWidth: 1,
+                                data: ${JSON.stringify(ydata)},
+                                stack: 1,
+                            },                        
+                        ]
+                    };
+
+                    var options = {scales: {yAxes: [{stacked: true}]}};
+
+                    var myBarChart = new Chart(document.querySelector('#canvas'), {
+                        type: 'bar',
+                        data: data,
+                        options: options
+                    });
+                </script>
+            </body>
+            </html>
+            `)
+        })
+        .catch(e => {
+            res.end(e);
+        })
+    }
+}
---
+++ index.js
@@ -0,0 +1,89 @@
+// yeah, let's start with that :)
+function die(msg, code) {
+    console.log(msg);
+    if (code === undefined) {
+        code = -1;
+    }
+
+    process.exit(code);
+}
+
+// input
+const argv = require('minimist')(process.argv.slice(2));
+const AUTHORIZATION_URL = argv.authorization_url || die("Missing 'authorization_url' argument.");
+const TOKEN_URL = argv.token_url || die("Missing 'token_url' argument.");
+const CLIENT_ID = argv.client_id || die("Missing 'client_id' argument.");
+const CLIENT_SECRET = argv.client_secret || die("Missing 'client_secret' argument.");
+
+const MY_HOST = 'localhost';
+const MY_PORT = 3000;
+const MY_ADDR = `${MY_HOST}:${MY_PORT}`
+const CALLBACK_URL = `http://${MY_ADDR}/callback`;
+
+
+// import express
+const url = require('url');
+const express = require('express');
+const session = require('express-session')({
+    secret: 'top_secret',
+    resave: true,
+    saveUninitialized: true,
+})
+
+
+// import passport library
+const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
+
+
+// setup passport
+passport.use(new OAuth2Strategy({
+        authorizationURL: AUTHORIZATION_URL,
+        tokenURL: TOKEN_URL,
+        clientID: CLIENT_ID,
+        clientSecret: CLIENT_SECRET,
+        callbackURL: CALLBACK_URL,
+    },
+    (accessToken, refreshToken, profile, cb) => cb(null, {"access_token": accessToken})
+));
+
+passport.serializeUser((user, done) => done(null, user));
+passport.deserializeUser((user, done) => done(null, user));
+
+
+// initialize app
+const app = express();
+const handlers = require('./handlers');
+
+app.use(session);
+app.use(passport.initialize());
+app.use(passport.session());
+
+const authenticator = passport.authenticate('oauth2', {
+    failureRedirect: '/login',
+    successReturnToOrRedirect: '/'
+})
+
+const require_authentication = function(req, res, next) {
+    if (!req.isAuthenticated()) {
+        req.session.returnTo = req.url;
+        return res.redirect('/login');
+    }
+
+    next();
+}
+
+// setup handlers
+app.get('/login', authenticator);
+app.get('/callback', authenticator);
+app.get('/logout', (req, res) => {
+    req.logout();
+    res.redirect('/');
+});
+
+app.get('/', handlers.indexHandler);
+app.get('/results/:id', require_authentication, handlers.displayResultsHandler);
+
+app.listen(MY_PORT, MY_HOST, function () {
+    console.log(`Running on http://${MY_ADDR}`);
+})
